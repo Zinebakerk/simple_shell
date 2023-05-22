40ddaadf5ac2f5259f8aaf2db595d182a110bb2e
Zineb <zineb.akerkkaou999@gmail.com>
2023-05-22 21:04:23 +0000

    3 functions added

--- main.h
+++ main.h
@@ -11,6 +11,8 @@
 void prints(char *str);
 char **read_cmd(void);
 void execmd(char **cmd, char *shell_name);
-
+char *read_cmd(void);
+char **split(char *buffer);
+char **read_split_cmd(void);
 #define PROMPT "#cisfun$ "
 #endif
