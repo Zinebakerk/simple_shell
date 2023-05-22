6c2753172219d3734cf49c84866b3c902aac6ab8
Zineb <zineb.akerkkaou999@gmail.com>
2023-05-22 20:55:45 +0000

    modification : added another function

--- tools.c
+++ tools.c
@@ -1,82 +1,93 @@
 #include "main.h"
 /**
- * prints - a function that print a string to stdout
- * str: the string to print
- *
- * Return: (void)
- */
+* prints - a function that print a string to stdout
+* str: the string to print
+*
+* Return: (void)
+*/
 void prints(char *str)
 {
-	int i = 0;
+int i = 0;
 
-	if (!str)
-		return;
-	while (str[i] != '\0')
-	{
-		write(STDOUT_FILENO, &str[i], 1);
-		i++;
-	}
+if (!str)
+return;
+while (str[i] != '\0')
+{
+write(STDOUT_FILENO, &str[i], 1);
+i++;
+}
 }
 /**
- * read_cmd - a function that reads command from the stdin and
- * split it to an array of each word of the string
- *
- * Return: (void)
- */
+* read_cmd - a function that reads command from the stdin and
+* split it to an array of each word of the string
+*
+* Return: (void)
+*/
 char *read_cmd(void)
 {
-	size_t size = 0;
-	ssize_t r_bytes;
-	char *buffer = NULL;
+size_t size = 0;
+ssize_t r_bytes;
+char *buffer = NULL;
 
-	r_bytes = getline(&buffer, &size, stdin);
-	if (r_bytes == -1)
-	{
-		exit(EXIT_FAILURE);
-	}
-	buffer[r_bytes - 1] = '\0';
-	return (buffer);
+r_bytes = getline(&buffer, &size, stdin);
+if (r_bytes == -1)
+{
+exit(EXIT_FAILURE);
 }
+buffer[r_bytes - 1] = '\0';
+return (buffer);
+}
+
+
 char **split(char *buffer)
 {
 char *dup, *token, **arr = NULL;
+int wc = 0;
+dup = strdup(buffer);
+token = strtok(dup, " \t\n");
+/* if there is no command */
+if (token == NULL)
+return (NULL);
 
-	dup = strdup(buffer);
-        int wc = 0;
-	token = strtok(dup, " \t\n");
-	/* if there is no command */
-	if (token == NULL)
-		return (NULL);
-
-	while (token != NULL)
-	{
-		wc++;
-		arr = realloc(arr, sizeof(char *) * (wc + 1));
-		if (arr == NULL)
-		{
-			free(dup);
-			perror("EROOR REALLOC");
-			exit(EXIT_FAILURE);
-		}
-		arr[wc - 1] = strdup(token);
-		token = strtok(NULL, " \t\n");
-	}
-	arr[wc] = NULL;
-	free(dup);
-	return (arr);
+while (token != NULL)
+{
+wc++;
+arr = realloc(arr, sizeof(char *) * (wc + 1));
+if (arr == NULL)
+{
+free(dup);
+perror("EROOR REALLOC");
+exit(EXIT_FAILURE);
 }
+arr[wc - 1] = strdup(token);
+token = strtok(NULL, " \t\n");
+}
+arr[wc] = NULL;
+free(dup);
+return (arr);
+}
+
+
+char **read_split_cmd(void)
+{
+char *buffer = NULL;
+buffer = read_cmd();
+return (split(buffer));
+}
+
+
 /**
- * execmd - a function that execute a command
- * @cmd: an array of strings of commands
- * @shell_name: the shell name
- *
- * Return: (void)
- */
+* execmd - a function that execute a command
+* @cmd: an array of strings of commands
+* @shell_name: the shell name
+*
+* Return: (void)
+*/
 void execmd(char **cmd, char *shell_name)
 {
-	if (execve(cmd[0], cmd, NULL) == -1)
-	{
-		perror(shell_name);
-		exit(EXIT_FAILURE);
-	}
+if (execve(cmd[0], cmd, NULL) == -1)
+{
+perror(shell_name);
+exit(EXIT_FAILURE);
+}
 }
