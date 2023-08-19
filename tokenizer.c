25ab0799bb986dc63ed40e6185b703e416c8dd36
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2023-08-19 06:54:10 +0100

    task1

--- tokenizer.c
+++ tokenizer.c
@@ -10,12 +10,12 @@ char **tokenizer(char *line)
 	tmp = _strdup(line);
 
 	token = strtok(tmp, DELIM);
-	/*if (token == NULL)
+	if (token == NULL)
 	{
 		free(line), line = NULL;
 		free(tmp), tmp = NULL;
 		return (NULL);
-	}*/
+	}
 	
 	while (token)
 	{
